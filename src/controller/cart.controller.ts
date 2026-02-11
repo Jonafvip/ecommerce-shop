d07a2b06020b111855f78fabad3bb4c6b981db4c
Jona <zardax.david98@gmail.com>
2026-02-11 15:26:17 -0600

    Creamos el controlador para eliminar un producto del carrito

--- src/controller/cart.controller.ts
+++ src/controller/cart.controller.ts
@@ -70,3 +70,50 @@ export const addProductToCart = async (req: Request, res: Response) => {
     });
   }
 };
+
+export const removeProductFromCart = async (req: Request, res: Response) => {
+  try {
+    const userId = req.user?.id;
+    const { id } = req.params;
+
+    if (!userId)
+      return res.status(401).json({ message: "User not authenticated" });
+
+    const productCart = await prisma.product.findFirst({
+      where: { id: Number(id) },
+    });
+
+    if (!productCart) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+
+    const cartProduct = await prisma.cart.deleteMany({
+      where: {
+        userId: userId!,
+        productId: Number(id),
+      },
+    });
+
+    if (cartProduct.count === 0) {
+      return res.status(404).json({
+        message: "The product was not found in the cart for this user.",
+      });
+    }
+    return res
+      .status(200)
+      .json({
+        message: "product disposed correctly",
+        deleteCount: cartProduct,
+      });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Internal server error",
+      error: error instanceof Error ? error.message : error,
+    });
+  }
+};
+
+
+export const emptyCart = async(req:Request,res:Response) => {
+
+}
