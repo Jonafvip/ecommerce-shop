078182bdb934e57e30d544e50780c992a6a753b8
Jona <zardax.david98@gmail.com>
2026-02-10 19:13:42 -0600

    Definimos el endpoint de registrarse

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,14 @@
 import express from "express";
 import { config } from "dotenv";
+import authRoute from "./routes/auth.route.js";
+import morgan from "morgan";
 
 config();
 const app = express();
 app.use(express.json());
+app.use(morgan("dev"));
+
+app.use("/api/v1/auth", authRoute);
 
 const PORT = process.env.PORT;
 
