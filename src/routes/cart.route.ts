10acf674d733d7042d667418070a0e26e17986ca
Jona <zardax.david98@gmail.com>
2026-02-11 15:26:39 -0600

    Creamos el endpoint para eliminar producto del carrito

--- src/routes/cart.route.ts
+++ src/routes/cart.route.ts
@@ -1,9 +1,13 @@
-import {Router} from 'express'
-import { verify } from '../middleware/auth.middleware.js'
-import { addProductToCart } from '../controller/cart.controller.js'
+import { Router } from "express";
+import { verify } from "../middleware/auth.middleware.js";
+import {
+  addProductToCart,
+  removeProductFromCart,
+} from "../controller/cart.controller.js";
 
-const cartRoute = Router()
-cartRoute.use(verify)
-cartRoute.post("/:id",addProductToCart)
+const cartRoute = Router();
+cartRoute.use(verify);
+cartRoute.post("/:id", addProductToCart);
+cartRoute.delete("/:id", removeProductFromCart);
 
-export default cartRoute
+export default cartRoute;
