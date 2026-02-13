e71a904a4380acf515957a1ffbd7c353b2e25928
Jona <zardax.david98@gmail.com>
2026-02-13 17:31:04 -0600

    Instalacion y implementacion de cors

--- src/server.ts
+++ src/server.ts
@@ -7,8 +7,17 @@ import cartRoute from "./routes/cart.route.js";
 import orderRoute from "./routes/order.route.js";
 import morgan from "morgan";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 const app = express();
+app.use(
+  cors({
+    credentials: true,
+    origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 app.use(morgan("dev"));
