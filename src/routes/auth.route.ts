76b76465901da15da5e5060a42b6934a653c6cea
Jona <zardax.david98@gmail.com>
2026-02-13 14:58:48 -0600

    Implementacion de zod para la validacion de datos de auth

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,8 +1,12 @@
 import { Router } from "express";
 import { login, logout, register } from "../controller/auth.controller.js";
+import {
+  registerValidation,
+  validationSchema,
+} from "../validation/auth.validation.js";
 
 const authRoute = Router();
-authRoute.post("/register", register);
+authRoute.post("/register", validationSchema(registerValidation), register);
 authRoute.post("/login", login);
 authRoute.post("/logout", logout);
 
---
+++ src/validation/auth.validation.ts
@@ -0,0 +1,42 @@
+import type { Request, Response, NextFunction } from "express";
+import { z, ZodError } from "zod";
+
+export const registerValidation = z.object({
+  username: z
+    .string()
+    .min(3, { error: "The username must be at least 3 characters long." })
+    .max(40, {
+      error: "The username is too long, it must be less than 40 characters.",
+    }),
+  email: z.string().email({ error: "email invalid" }),
+  password: z
+    .string()
+    .min(6, { error: "The password must be at least 6 characters long" })
+    .max(20, { error: "The password must be less than 20 characters." }),
+  role: z.string().default("USER"),
+});
+
+export const loginValidation = z.object({
+  email: z.string().email(),
+  password: z.string().min(1, { error: "Password is required" }),
+});
+
+export const validationSchema =
+  (schema: z.ZodObject<any>) =>
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      req.body = await schema.parseAsync(req.body);
+      return next();
+    } catch (error) {
+      if (error instanceof ZodError) {
+        return res.status(400).json({
+          status: "fail",
+          errors: error.issues.map((issue) => ({
+            path: issue.path,
+            message: issue.message,
+          })),
+        });
+      }
+      return res.status(500).json({ message: "Internal Server Error" });
+    }
+  };
