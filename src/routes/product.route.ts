8a96ba250491b9c2b0563420fc41ec504670f93f
Jona <zardax.david98@gmail.com>
2026-02-13 12:49:46 -0600

    Creacion del endpoint de actualizar y eliminar producto

--- src/routes/product.route.ts
+++ src/routes/product.route.ts
@@ -1,10 +1,16 @@
 import { Router } from "express";
 import { verify, isAdmin } from "../middleware/auth.middleware.js";
-import { createProducts } from "../controller/product.controller.js";
+import {
+  createProducts,
+  deleteProduct,
+  updateProduct,
+} from "../controller/product.controller.js";
 
 const productRoute = Router();
 
 productRoute.use(verify, isAdmin);
 productRoute.post("/", createProducts);
+productRoute.put("/:id", updateProduct);
+productRoute.delete("/:id", deleteProduct);
 
 export default productRoute;
