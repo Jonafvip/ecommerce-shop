12e2374eedf6a8a9e7472cb33eb654f87df720ef
Jona <zardax.david98@gmail.com>
2026-02-16 01:09:50 -0600

    Refactorizacion de productController

--- src/controller/product.controller.ts
+++ src/controller/product.controller.ts
@@ -1,50 +1,34 @@
 import { prisma } from "../config/prisma.config.js";
-import { Prisma } from "../generated/prisma/client.js";
 import type { Request, Response } from "express";
+import { catchAsync } from "../middleware/errorHandlerMiddleware.js";
+import { AppError } from "../errors/server.error.js";
 
-export const getAllProducts = async (req: Request, res: Response) => {
-  try {
+export const getAllProducts = catchAsync(
+  async (req: Request, res: Response) => {
     const product = await prisma.product.findMany({});
-    if (!product) return res.status(404).json({ message: "Product not found" });
+    if (!product) throw new AppError("Product not found", 404);
 
-    return res.status(200).json({  product , success: true });
-  } catch (error) {
-    console.log("Product Error", error);
-    return res.status(500).json({
-      message: "Internal server error",
-      error: error instanceof Error ? error.message : error,
-    });
+    res.status(200).json({ product, success: true });
   }
-};
+);
 
-export const getProductId = async (req: Request, res: Response) => {
-  try {
-    const { id } = req.params;
-    const productId = await prisma.product.findUnique({
-      where: { id: Number(id) },
-    });
+export const getProductId = catchAsync(async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const productId = await prisma.product.findUnique({
+    where: { id: Number(id) },
+  });
 
-    if (!productId)
-      return res.status(404).json({ message: "Product not found" });
+  if (!productId) throw new AppError("Product not found", 404);
 
-    return res.status(200).json({ data: { productId } });
-  } catch (error) {
-    console.log("Product Error", error);
-    return res.status(500).json({
-      message: "Internal server Error",
-      error: error instanceof Error ? error.message : error,
-    });
-  }
-};
+  res.status(200).json({ data: { productId } });
+});
 
-export const createProducts = async (req: Request, res: Response) => {
-  try {
+export const createProducts = catchAsync(
+  async (req: Request, res: Response) => {
     const { name, description, stock, price } = req.body;
 
     if (!name || !description || !stock || !price)
-      return res
-        .status(400)
-        .json({ message: "Please fill in the empty fields" });
+      throw new AppError("Please fill in the empty fields", 400);
 
     const newProduct = await prisma.product.create({
       data: {
@@ -55,91 +39,48 @@ export const createProducts = async (req: Request, res: Response) => {
       },
     });
 
-    return res.status(201).json({
+    res.status(201).json({
       message: "successfully created",
       data: { newProduct },
       success: true,
     });
-  } catch (error: unknown) {
-    if (error instanceof Prisma.PrismaClientKnownRequestError) {
-      console.log("Error of Prisma:", error.code);
-      return;
-    } else if (error instanceof Error) {
-      return res.json({
-        message: "The user could not be registered.",
-        error: error.message,
-      });
-    } else {
-      return res.json({ message: "Internal server problems", error: error });
-    }
   }
-};
+);
 
 //pendiente con las relaciones de restricciones
-export const deleteProduct = async (req: Request, res: Response) => {
-  try {
-    const { id } = req.params;
-    const productId = await prisma.product.delete({
-      where: { id: Number(id) },
-    });
+export const deleteProduct = catchAsync(async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const productId = await prisma.product.delete({
+    where: { id: Number(id) },
+  });
 
-    if (!productId)
-      return res.status(404).json({ message: "Product not found" });
+  if (!productId) throw new AppError("Product not found", 404);
 
-    return res
-      .status(200)
-      .json({ message: "product successfully removed", success: true });
-  } catch (error) {
-    if (error instanceof Prisma.PrismaClientKnownRequestError) {
-      console.log("Error of Prisma:", error.code);
-      return;
-    } else if (error instanceof Error) {
-      return res.status(500).json({
-        message: "The product could not be removed.",
-        error: error.message,
-      });
-    } else {
-      return res.json({ message: "Internal server problems", error: error });
-    }
-  }
-};
+  res
+    .status(200)
+    .json({ message: "product successfully removed", success: true });
+});
 
-export const updateProduct = async (req: Request, res: Response) => {
-  try {
-    const { id } = req.params;
-    const { name, description, stock, price } = req.body;
+export const updateProduct = catchAsync(async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const { name, description, stock, price } = req.body;
 
-    if (!name || !description || !stock || !price)
-      return res
-        .status(400)
-        .json({ message: "Please fill in the empty fields" });
+  if (!name || !description || !stock || !price)
+    throw new AppError("Please fill in the empty fields", 400);
 
-    const upProduct = await prisma.product.update({
-      where: { id: Number(id) },
-      data: {
-        name,
-        description,
-        stock,
-        price,
-      },
-    });
+  const upProduct = await prisma.product.update({
+    where: { id: Number(id) },
+    data: {
+      name,
+      description,
+      stock,
+      price,
+    },
+  });
 
-    return res.status(200).json({
-      message: "successfully updated product",
-      success: true,
-      data: { upProduct },
-    });
-  } catch (error) {
-    if (error instanceof Prisma.PrismaClientKnownRequestError) {
-      console.log("Error of Prisma:", error.code);
-      return;
-    } else if (error instanceof Error) {
-      return res.status(500).json({
-        message: "The product could not be update.",
-        error: error.message,
-      });
-    } else {
-      return res.json({ message: "Internal server problems", error: error });
-    }
-  }
-};
+  res.status(200).json({
+    message: "successfully updated product",
+    success: true,
+    data: { upProduct },
+  });
+});
