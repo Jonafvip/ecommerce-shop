d0d30bec38656d56c35599624b65004775b60613
Jona <zardax.david98@gmail.com>
2026-02-16 01:07:30 -0600

    Refactorizacion de authController

--- src/controller/auth.controller.ts
+++ src/controller/auth.controller.ts
@@ -1,126 +1,83 @@
 import { prisma } from "../config/prisma.config.js";
-import { Prisma } from "../generated/prisma/client.js";
 import bcrypt from "bcrypt";
 import type { Request, Response } from "express";
 import { authToken } from "../utils/auth.utils.js";
+import { AppError } from "../errors/server.error.js";
+import { catchAsync } from "../middleware/errorHandlerMiddleware.js";
 
-export const register = async (req: Request, res: Response) => {
-  try {
-    const { username, email, password, role } = req.body;
+export const register = catchAsync(async (req: Request, res: Response) => {
+  const { username, email, password, role } = req.body;
 
-    if (!username || !email || !password) {
-      return res
-        .status(400)
-        .json({ message: "Please fill in the empty fields" });
-    }
-
-    const userExisting = await prisma.user.findFirst({
-      where: { OR: [{ email: email }, { username: username }] },
-    });
-
-    if (userExisting) {
-      return res.status(400).json({ message: "This user already exists" });
-    }
-
-    const saltRound = 10;
-    const hashedPassword = await bcrypt.hash(password, saltRound);
-
-    const newUser = await prisma.user.create({
-      data: {
-        username,
-        email,
-        password: hashedPassword,
-        role,
-      },
-    });
-
-    return res.status(201).json({
-      message: "successfully registered user",
-      data: {
-        id: newUser.id,
-        username: newUser.username,
-        email: newUser.email,
-        createdAt: newUser.createdAt,
-      },
-      success: true,
-    });
-  } catch (error: unknown) {
-    if (error instanceof Prisma.PrismaClientKnownRequestError) {
-      console.log("Error of Prisma:", error.code);
-      return;
-    } else if (error instanceof Error) {
-      return res.json({
-        message: "The user could not be registered.",
-        error: error.message,
-      });
-    } else {
-      return res.json({ message: "Internal server problems", error: error });
-    }
+  if (!username || !email || !password) {
+    throw new AppError("Please fill in the empty fields", 404);
   }
-};
 
-export const login = async (req: Request, res: Response) => {
-  try {
-    const { email, password } = req.body;
+  const userExisting = await prisma.user.findFirst({
+    where: { OR: [{ email: email }, { username: username }] },
+  });
 
-    if (!email || !password) {
-      return res
-        .status(400)
-        .json({ message: "Please fill in the empty fields" });
-    }
-
-    const userExisting = await prisma.user.findFirst({
-      where: { email: email },
-    });
-
-    if (!userExisting) {
-      return res.status(400).json({ message: "Invalid user" });
-    }
-
-    const isMatch = await bcrypt.compare(password, userExisting.password);
-    if (!isMatch) return res.status(400).json({ message: "Invalid user" });
-
-    const token = authToken(userExisting.id, userExisting.role);
-
-    res.cookie("jwt", token, { httpOnly: true, sameSite: "strict" });
-
-    return res.status(200).json({
-      message: "Successful login",
-      success: true,
-      data: {
-        id: userExisting.id,
-        email: userExisting.email,
-        role: userExisting.role,
-      },
-      token,
-    });
-  } catch (error: unknown) {
-    if (error instanceof Prisma.PrismaClientKnownRequestError) {
-      console.log("Error of Prisma:", error.code);
-      return;
-    } else if (error instanceof Error) {
-      return res.json({
-        message: "The user could not be registered.",
-        error: error.message,
-      });
-    } else {
-      return res.json({ message: "Internal server problems", error: error });
-    }
+  if (userExisting) {
+    throw new AppError("This user already exists", 400);
   }
-};
 
-export const logout = async (req: Request, res: Response) => {
-  try {
-    res.clearCookie("jwt");
-    return res.status(200).json({ message: "Successful session closure" });
-  } catch (error) {
-    if (error instanceof Error) {
-      return res.json({
-        message: "The session could not be closed.",
-        error: error.message,
-      });
-    } else {
-      return res.json({ message: "Internal server problems", error: error });
-    }
+  const saltRound = 10;
+  const hashedPassword = await bcrypt.hash(password, saltRound);
+
+  const newUser = await prisma.user.create({
+    data: {
+      username,
+      email,
+      password: hashedPassword,
+      role,
+    },
+  });
+
+  res.status(201).json({
+    message: "successfully registered user",
+    data: {
+      id: newUser.id,
+      username: newUser.username,
+      email: newUser.email,
+      createdAt: newUser.createdAt,
+    },
+    success: true,
+  });
+});
+
+export const login = catchAsync(async (req: Request, res: Response) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    throw new AppError("Email and password are required", 400);
   }
-};
+
+  const userExisting = await prisma.user.findUnique({
+    where: { email: email },
+  });
+
+  if (!userExisting) {
+    throw new AppError("Invalid credentials", 401);
+  }
+
+  const isMatch = await bcrypt.compare(password, userExisting.password);
+  if (!isMatch) throw new AppError("Invalid user", 400);
+
+  const token = authToken(userExisting.id, userExisting.role);
+
+  res.cookie("jwt", token, { httpOnly: true, sameSite: "strict" });
+
+  res.status(200).json({
+    message: "Successful login",
+    success: true,
+    data: {
+      id: userExisting.id,
+      email: userExisting.email,
+      role: userExisting.role,
+    },
+  });
+});
+
+export const logout = catchAsync(async (req: Request, res: Response) => {
+  res.clearCookie("jwt");
+  return res.status(200).json({ message: "Successful session closure" });
+});
