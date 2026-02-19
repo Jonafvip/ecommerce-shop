9ea566f2926bc3290b1cda2cdd4debff1a08e6ad
Jona <zardax.david98@gmail.com>
2026-02-19 17:18:36 -0600

    Envolvemos la app por el contexto

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,18 +4,24 @@ import { Home } from "./pages/Home";
 import { Layout } from "./layout/layout";
 import { Register } from "./pages/auth/Register";
 import { Login } from "./pages/auth/Login";
+import { ContextProvider } from "./context/AuthContext";
+
+import { Toaster } from "react-hot-toast";
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Layout />}/>
+      <ContextProvider>
+        <Toaster position="top-center" reverseOrder={false} />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Layout />} />
             <Route index element={<Home />} />
             <Route path="/register" element={<Register />} />
             <Route path="/login" element={<Login />} />
-        </Routes>
-      </BrowserRouter>
+          </Routes>
+        </BrowserRouter>
+      </ContextProvider>
     </>
   );
 }
