cf1a279a4f8bb8879ca98d686de4f85b8a353aa1
Jona <zardax.david98@gmail.com>
2026-02-20 17:24:52 -0600

    Agreamos una funcionalidad donde se van mostrando los los productos agregados al carrito

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,11 +1,29 @@
 import { useAuthContext } from "../context/AuthContext";
+import { useCartContext } from "../context/CartContext";
 import "./Header.css";
 import { NavLink } from "react-router-dom";
 //componentes
 import Stack from "@mui/material/Stack";
 import Button from "@mui/material/Button";
+
+//
+import Badge from "@mui/material/Badge";
+import { styled } from "@mui/material/styles";
+import IconButton from "@mui/material/IconButton";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+
+const StyledBadge = styled(Badge)(({ theme }) => ({
+  "& .MuiBadge-badge": {
+    right: -3,
+    top: 13,
+    border: `2px solid ${(theme.vars ?? theme).palette.background.paper}`,
+    padding: "0 4px",
+  },
+}));
+
 export const Header = () => {
   const { user, onLogout } = useAuthContext();
+  const { cartCount } = useCartContext();
 
   return (
     <header className="container-header">
@@ -17,27 +35,38 @@ export const Header = () => {
         </h2>
         <nav>
           <ul>
-            <ul>
-              {user ? (
-                <Stack spacing={2} direction="row">
-                  <NavLink to="/profile">
-                    <Button variant="text">Profile</Button>
-                  </NavLink>
-                  <Button variant="text" onClick={onLogout}>
-                    Logout
-                  </Button>
-                </Stack>
-              ) : (
-                <li>
-                  <NavLink
-                    style={{ textDecoration: "none", fontWeight: "bold" }}
-                    to="/register"
-                  >
-                    Log in
-                  </NavLink>
-                </li>
-              )}
-            </ul>
+            {user ? (
+              <Stack spacing={2} direction="row" alignItems="center">
+                <NavLink
+                  to="/cart"
+                  style={{ color: "inherit", textDecoration: "none" }}
+                >
+                  <IconButton aria-label="cart" color="inherit">
+                    <StyledBadge badgeContent={cartCount} color="secondary">
+                      <ShoppingCartIcon />
+                    </StyledBadge>
+                  </IconButton>
+                </NavLink>
+                <NavLink to="/profile" style={{ textDecoration: "none" }}>
+                  <Button variant="text">Profile</Button>
+                </NavLink>
+                <Button variant="text" onClick={onLogout}>
+                  Logout
+                </Button>
+              </Stack>
+            ) : (
+              <li>
+                <NavLink
+                  style={{
+                    textDecoration: "none",
+                    fontWeight: "bold",
+                  }}
+                  to="/register"
+                >
+                  Log in
+                </NavLink>
+              </li>
+            )}
           </ul>
         </nav>
       </div>
