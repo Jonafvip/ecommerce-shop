50e001c5583119970d3028827ae4df893c207d68
Jona <zardax.david98@gmail.com>
2026-02-20 17:04:19 -0600

    Envolvemos app con el CartProvider

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,33 +5,45 @@ import { Layout } from "./layout/layout";
 import { Register } from "./pages/auth/Register";
 import { Login } from "./pages/auth/Login";
 import { ContextProvider } from "./context/AuthContext";
+import { CartProvider } from "./context/CartContext";
 
 import { Toaster } from "react-hot-toast";
 import { Profile } from "./pages/Profile";
+import { Cart } from "./pages/cart/Cart";
 import { ProtectedRoute } from "./routes/ProtectedRoute";
 
 function App() {
   return (
     <>
       <ContextProvider>
-        <Toaster position="top-center" reverseOrder={false} />
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Layout />} />
-            <Route index element={<Home />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/login" element={<Login />} />
+        <CartProvider>
+          <Toaster position="top-center" reverseOrder={false} />
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<Layout />} />
+              <Route index element={<Home />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/login" element={<Login />} />
 
-            <Route
-              path="/profile"
-              element={
-                <ProtectedRoute>
-                  <Profile />
-                </ProtectedRoute>
-              }
-            />
-          </Routes>
-        </BrowserRouter>
+              <Route
+                path="/profile"
+                element={
+                  <ProtectedRoute>
+                    <Profile />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/cart"
+                element={
+                  <ProtectedRoute>
+                    <Cart />
+                  </ProtectedRoute>
+                }
+              />
+            </Routes>
+          </BrowserRouter>
+        </CartProvider>
       </ContextProvider>
     </>
   );
