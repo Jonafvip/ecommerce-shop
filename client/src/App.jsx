79cfac9c26f303d6578b3f31f5a05d9b545ba9c5
Jona <zardax.david98@gmail.com>
2026-02-20 14:26:18 -0600

    Creamos una ruta protegida

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,8 @@ import { Login } from "./pages/auth/Login";
 import { ContextProvider } from "./context/AuthContext";
 
 import { Toaster } from "react-hot-toast";
+import { Profile } from "./pages/Profile";
+import { ProtectedRoute } from "./routes/ProtectedRoute";
 
 function App() {
   return (
@@ -19,6 +21,15 @@ function App() {
             <Route index element={<Home />} />
             <Route path="/register" element={<Register />} />
             <Route path="/login" element={<Login />} />
+
+            <Route
+              path="/profile"
+              element={
+                <ProtectedRoute>
+                  <Profile />
+                </ProtectedRoute>
+              }
+            />
           </Routes>
         </BrowserRouter>
       </ContextProvider>
---
+++ client/src/routes/ProtectedRoute.jsx
@@ -0,0 +1,8 @@
+import { Navigate } from "react-router-dom";
+import { useAuthContext } from "../context/AuthContext";
+
+export const ProtectedRoute = ({ children }) => {
+  const { user } = useAuthContext();
+  if (!user) return <Navigate to="/login" />;
+  return children;
+};
